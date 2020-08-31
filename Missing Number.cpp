0fa0b99d79be4e0a89f417c881e23cc190454adc
THe xD <70330230+Ii-xD-iI@users.noreply.github.com>
2020-08-31 23:36:21 +0530

    Create Missing Number.cpp

---
+++ Missing Number.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define int long long 
+#define all(x) begin(x),end(x)
+#define endl "\n"
+using namespace std;
+ 
+signed main(){
+ 
+    int t; cin >> t;
+    int sum{0};
+    for(int i {1}; i<t; i++){
+        int x; cin >> x;
+        sum += x;
+    }
+    cout << (int)((t*(t+1))>>1)-sum << endl;
+    
